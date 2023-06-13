093188e758fb909960ec01a461aca8ff0e5aa21f
imrongc <imron@gajicermat.com>
2023-06-13 15:58:38 +0700

    Updates

---
+++ icons/index.ts
@@ -0,0 +1,6 @@
+export {default as AccountIcon} from './account.svg';
+export {default as CheckCircleOutlineIcon} from './checkCircleOutline.svg';
+export {default as CloseCircleOutlineIcon} from './closeCircleOutline.svg';
+export {default as FaceIdIcon} from './faceId.svg';
+export {default as HomeIcon} from './home.svg';
+export {default as InfoCircleIcon} from './infoCircle.svg';
